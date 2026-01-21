0b6dfc9ef0fa83f2e0384db01524626641deda30
p12010304 <e1481848@u.nus.edu>
2026-01-21 21:26:13 +0800

    Level 1. Echo

--- src/main/java/Bob.java
+++ src/main/java/Bob.java
@@ -1,10 +1,30 @@
+import java.util.Scanner;
+
 public class Bob {
     public static void main(String[] args) {
-        System.out.println("____________________________________________________________\n" +
-                " Hello! I'm Bob\n" +
-                " What can I do for you?\n" +
-                "____________________________________________________________\n" +
-                " Bye. Hope to see you again soon!\n" +
-                "____________________________________________________________\n");
+        String line = "____________________________________________________________";
+
+        System.out.println(line);
+        System.out.println(" Hello! I'm Bob");
+        System.out.println(" What can I do for you?");
+        System.out.println(line);
+
+        Scanner in = new Scanner(System.in);
+        String input;
+
+        while (true) {
+            input = in.nextLine();
+
+            if (input.equals("bye")) {
+                System.out.println(line);
+                System.out.println(" Bye. Hope to see you again soon!");
+                System.out.println(line);
+                break;
+            }
+
+            System.out.println(line);
+            System.out.println(" " + input);
+            System.out.println(line);
+        }
     }
 }
